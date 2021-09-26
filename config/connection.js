5bcf1b209b8cd0a209b950b03961d5aa0fdbace5
aclar <a.clarke0307@gmail.com>
2021-09-25 21:53:08 -0400

    connect to jawdb

--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,18 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
+let sequelize
+
+
 //connect to database
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+if(process.env.JAWDB_URL){
+    sequelize = new sequelize(process.env.JAWDB_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
-
 });
+}
 
 module.exports = sequelize;
